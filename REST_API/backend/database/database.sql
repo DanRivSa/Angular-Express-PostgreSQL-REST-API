22c93a23afcc572cb1077601064cfff622ebd5e5
Daniel Rivero <danielriver2000@gmail.com>
2020-05-20 21:42:55 -0400

    init2

--- REST_API/backend/database/database.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE rest_api;
-
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY NOT NULL,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    username VARCHAR(50) NOT NULL
-);
-
